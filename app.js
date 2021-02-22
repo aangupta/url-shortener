e2f990a12cc7dafb8ff924225b5e0e689c8acbe1
aangupta <aanchalgupta457@gmail.com>
2021-02-22 16:09:06 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const api = require('./api');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.listen(port, () => {
+    console.log(`Server listening at port: ${port}`);
+});
---
+++ urls.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const shortid = require('shortid');
+
+const router = express.Router();
+
+const urls = {};
+
+router.get('/', (req, res) => {
+    const urlList = [];
+    Object.keys(urls).forEach((urlID) => {
+        urlList.push({ id: urlID, longUrl: urls[urlID] });
+    });
+    res.send(urlList);
+});
+
+router.post('/', (req, res) => {
+    /* const { longUrl } = req.body.longUrl; */
+    /* console.log(longUrl); */
+    const id = shortid.generate();
+    urls[id] = req.body.longUrl;
+    res.status(201).send({ id });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const longUrl = urls[req.params.id];
+    if (longUrl) {
+        res.status(200).send({
+            id,
+            longUrl,
+        });
+    } else {
+        res.status(404).send('url shortener id not found');
+    }
+});
+
+module.exports = router;
