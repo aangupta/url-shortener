a5bdb185d247e0e817ad0630933d24b90ccd5eea
aangupta <aanchalgupta457@gmail.com>
2021-02-24 11:13:30 +0530

    changed the css file location and updated url name

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -50,7 +50,7 @@ function printurl() {
 }
 
 document.querySelector('#search').addEventListener('click', () => {
-    const url = new URL('/api/urls/', 'https://url-shortform.herokuapp.com');
+    const url = new URL('/api/urls/', 'https://curtly.herokuapp.com/');
     const searchurl = document.querySelector('#searchurl');
     const param = { id: searchurl.value };
     Object.keys(param).forEach((key) => {
