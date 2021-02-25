e10b3479d574b2bd01373a0ad46b52785f8235cf
aangupta <aanchalgupta457@gmail.com>
2021-02-25 21:45:44 +0530

    added the redirect function

--- app.js
+++ app.js
@@ -2,13 +2,24 @@
 const express = require('express');
 const api = require('./api');
 
+const { urls } = require('./urls_data');
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use('/api', api);
-/* app.use('/u/:id', (req, res) => ) */
+
+app.get('/u/:id', (req, res) => {
+    const longUrl = urls[req.params.id];
+    if (longUrl) {
+        res.redirect(longUrl);
+    } else {
+        res.status(404).send();
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`);
 });
